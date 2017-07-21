a4dd7002fc9517d27b12834359bd652e5cc860d4
aracelivegamagro <aracelivegamagro@hotmail.com>
2017-07-21 12:57:08 +0200

    deleted components

--- components/speechGoogleComp/bin/startGoogleSpeechComp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./googlespeechcomp --Ice.Config=../etc/generic_config,../etc/specific_config &
--- components/ursusCommonJoint/bin/startUrsusCommonJointComp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./ursuscommonjointcomp --Ice.Config=../etc/generic_config,../etc/specific_config &
