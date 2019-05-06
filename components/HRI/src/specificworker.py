1ba731d507ea408da4733c75d0c0c416b6c09d73
Lobato <carloslobatog@gmail.com>
2019-05-06 19:00:24 +0200

    cambiando la ruta de las credenciales del proyecto

--- components/HRI/src/specificworker.py
+++ components/HRI/src/specificworker.py
@@ -22,6 +22,7 @@ import audio_player
 
 from PySide import QtGui, QtCore
 from genericworker import *
+from ui_mainUI import *
 
 # If RoboComp was compiled with Python bindings you can use InnerModel in Python
 # sys.path.append('/opt/robocomp/lib')
@@ -44,7 +45,7 @@ class SpecificWorker(GenericWorker):
 		#	print "Error reading config params"
 		return True
 
-	@QtCore.Slot()
+	@QtCore.pyqtSlot()
 	def compute(self):
 		print 'SpecificWorker.compute...'
 		import argparse
@@ -57,37 +58,63 @@ class SpecificWorker(GenericWorker):
 
 		# credentianls for the grpc stub
 		import os
-		os.environ["GOOGLE_APPLICATION_CREDENTIALS"] ="/home/lobato/Documentos/Shelly-58088bc904c2.json"
+		#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] ="/home/lobato/Documentos/Shelly-58088bc904c2.json"
+		os.environ["GOOGLE_APPLICATION_CREDENTIALS"] ="/home/robocomp/robocomp/components/robocomp-araceli/components/HRI/Shelly-58088bc904c2.json"
 		project_id = 'shelly-e0a42'
 		session_id = '000001'
 		audio_file_path = "/home/robocomp/robocomp/components/robocomp-araceli/components/HRI/microphone-results.wav"
 		language_code = 'es-ES'
 
+		import dialogflow_v2beta1 as dialogflow
+		import text_to_texttospeech_response as ttts
 
-		situation = 0
-		#one unique person blocking the route
-		if situation == 1:
-			context = "Aequam memento rebus" "in arduis servare mentem"
-		#several people blocking the path
-		if situation == 2:
-			context = "Alohomora"
+		import dialogflow_v2beta1 as dialogflow
+		import audio_to_texttospeech_response as atts
 
-		if situation != 0:
-			situation = 0
-			# obtain the response from google DialogFlow
-			import dialogflow_v2beta1 as dialogflow
-			import text_to_texttospeech_response as ttts
-			session_client = dialogflow.SessionsClient()
-
-			response = ttts.text_to_texttospeech_response(project_id, session_id, context, language_code)
-			intent_detected = response.query_result.intent.display_name
-			audio_player.audio_player("output.wav")
-
-		else: print('sigamos')
+		session_client = dialogflow.SessionsClient()
 
 
-		flag = False
-		while flag == False:
+		##################################################
+		############### KEYWORD SPOTTING #################
+		mode = 0
+		while mode == 0:
+
+			from pocketsphinx import LiveSpeech, get_model_path
+			model_path = get_model_path()
+			speech = LiveSpeech(
+			    verbose=False,
+			    sampling_rate=16000,
+			    buffer_size=4096,
+			    no_search=False,
+			    full_utt=False,
+			    hmm=os.path.join(model_path, 'es'),
+			    lm=os.path.join(model_path, 'es-20k.lm.gz'),
+			    dic=os.path.join(model_path, 'es.dict')
+			)
+
+			for phrase in speech:
+					cadena = phrase.hypothesis()
+					if cadena.find('hola lisa') == -1:
+						print"A la espera..."
+					else:
+						print "A su servicio!"
+						# import pygame to play the resulted voice audio
+						import pygame
+						pygame.init()
+						pygame.mixer.music.load("buenas.wav")
+						pygame.mixer.music.play()
+						mode = 1
+						situation = 0
+						break
+
+					print(phrase)
+		##################################################
+		##################################################
+
+
+		##################################################
+		###############   MODO DIALOGFLOW   ##############
+		while mode == 1:
 			# obtain audio from the microphone: to capture the audio chunks
 			import speech_recognition as sr
 			r = sr.Recognizer()
@@ -95,15 +122,28 @@ class SpecificWorker(GenericWorker):
 				print("Di algo!")
 				audio = r.listen(source)
 
+			#one unique person blocking the route
+			if situation == 1:
+				context = "Aequam memento rebus" "in arduis servare mentem"
+			#several people blocking the path
+			if situation == 2:
+				context = "Alohomora"
+
+			if situation != 0:
+				situation = 0
+				# obtain the response from google DialogFlow
+				response = ttts.text_to_texttospeech_response(project_id, session_id, context, language_code)
+				intent_detected = response.query_result.intent.display_name
+				audio_player.audio_player("output.wav")
+
+			else: print('sigamos')
+
 			# write audio to a WAV file
 			# audiodata_instance.get_wav_data(convert_rate = None, convert_width = None)
 			with open("microphone-results.wav", "wb") as f:
 				f.write(audio.get_wav_data())
 
 			# obtain the response from google DialogFlow
-			import dialogflow_v2beta1 as dialogflow
-			import audio_to_texttospeech_response as atts
-
 			session_client = dialogflow.SessionsClient()
 			response = atts.audio_to_texttospeech_response(project_id, session_id, audio_file_path, language_code)
 			intent_detected = response.query_result.intent.display_name
@@ -127,7 +167,6 @@ class SpecificWorker(GenericWorker):
 			# detect the intent of goodbye to exit the loop
 			despedida = 'charla.despedida'
 			if intent_detected == despedida:
-				time.sleep(10)
-				break
+				mode = 0
 
 		return True
